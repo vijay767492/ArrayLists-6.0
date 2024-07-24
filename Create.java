138d03c3b549f809b9f07c7b0f5cc2e8a648794f
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 11:40:35 +0530

    Create Create.java

---
+++ Create.java
@@ -0,0 +1,7 @@
+public class Create{
+public static void main(String[]args){
+// Create an Object for ArrayList
+  // Load Values in ArrayList
+  // Iterate it by usig Iterator and While Loops
+}
+}
